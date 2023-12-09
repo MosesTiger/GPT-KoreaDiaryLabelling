d95335415f7f29d6855be1ef48a06b1932f366b3
Hongseok Oh <97136787+abzb1@users.noreply.github.com>
2023-12-09 16:17:27 +0900

    Update model_real.py

--- model_real.py
+++ model_real.py
@@ -4,66 +4,25 @@ from transformers import AutoTokenizer, AutoModel
 import json
 import sys
 sys.path.append('/content/drive/My Drive/data')
-from model import BaseRegressor, SmallRegressor, LargeRegressor
+from utils.model import WeekChallenge
 
-class WeekChallenge(nn.Module):
-    
-    def __init__(self, model_args):
-        super(WeekChallenge, self).__init__()
-
-        self.args = model_args
-
-        self.tokenizer = AutoTokenizer.from_pretrained("monologg/kobigbird-bert-base", model_max_length=512)
-        self.embed = AutoModel.from_pretrained("monologg/kobigbird-bert-base")        
-        
-        if self.args["model_size"] == "base":
-            self.regressor = BaseRegressor()
-        elif self.args["model_size"] == "small":
-            self.regressor = SmallRegressor()
-        elif self.args["model_size"] == "large":
-            self.regressor = LargeRegressor()
-
-        if self.args["use_cuda"]:
-            self.regressor = self.regressor.cuda()
-            self.embed = self.embed.cuda()
-
-    # 사용자 입력 처리
-    def process_input(self, user_input):
-        tokenized = self.tokenizer(user_input, return_tensors="pt", padding="max_length", truncation=True)
-        return tokenized
-
-    # 예측 함수
-    def predict_scores(self, user_input):
-        self.eval()
-        with torch.no_grad():
-            inputs = self.process_input(user_input)
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            outputs = self.embed(**inputs)
-            # 마지막 은닉 상태를 regressor에 전달
-            last_hidden_state = outputs.last_hidden_state[:, 0]
-            scores = self.regressor(last_hidden_state)
-            return scores
-
-    # 평가 함수
-    def evaluate_stars(self, pooler_output):
-        avg_pool = pooler_output.mean(dim=1)
-        # 0과 1 사이의 값으로 정규화
-        normalized_score = torch.sigmoid(avg_pool)
-        # 1에서 5 사이의 점수로 변환
-        stars = 1 + 4 * normalized_score
-        return stars
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_path = '/content/drive/My Drive/ckpt/4_Adam_0.01_base.pth'
-model_args = {"model_size": "base", "use_cuda": torch.cuda.is_available()}
+model_path = '/home1/ohs/ongoing_proj/GPT-KoreaDiaryLabelling/ckpt/4_Adam_0.001_small.pth'
+model_args = {"model_size": "small", "use_cuda": torch.cuda.is_available()}
+
 mynet = WeekChallenge(model_args).to(device)
 mynet.load_state_dict(torch.load(model_path, map_location=device))
 
 # 사용자로부터 일기 내용 입력받기
-user_diary_input = input("Enter your weekly diary: ")
-
+user_diary_input = """
+1000만원 짜리 예금 들었다.
+은행가서 적금도 들었다.
+그래도 오늘 운동을 못갔다. 아파서 운동을 못했다. 운동을 하고 나서 더 아픈 것 같아서 쉬었다.
+그리고 나서 책을 읽었다.
+"""
 # 점수 예측
-predicted_scores = mynet.predict_scores(user_diary_input)
+predicted_scores = mynet(user_diary_input)
 
 # 결과 출력
 print(f"Predicted Scores: {predicted_scores}")
